b6bcd2144f8045b35e4608cc8afe060fd16a8c86
julseng <jlsronquillo3@gmail.com>
2025-10-24 16:50:28 +0800

    Implement LocalFileAdapter for media source handling

---
+++ plugins/localfile/LocalFileAdapter.java
@@ -0,0 +1,24 @@
+package media;
+
+public class LocalFileAdapter implements IMediaSource {
+    private String fileName;
+
+    public LocalFileAdapter(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void open() {
+        System.out.println("Opening local file: " + fileName);
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing local file: " + fileName);
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closing local file: " + fileName);
+    }
+}
