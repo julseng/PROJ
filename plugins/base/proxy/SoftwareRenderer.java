aa7039893ea1f6603e7ec257470d490d7d741a4b
julseng <jlsronquillo3@gmail.com>
2025-10-24 16:45:59 +0800

    Add SoftwareRenderer class for rendering data

---
+++ plugins/base/proxy/SoftwareRenderer.java
@@ -0,0 +1,8 @@
+package media;
+
+public class SoftwareRenderer implements Renderer {
+    @Override
+    public void render(String data) {
+        System.out.println("[Software Renderer] " + data);
+    }
+}
