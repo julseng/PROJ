0ad2960b347389ce35ec62bb317c87c93d6b4c58
julseng <jlsronquillo3@gmail.com>
2025-10-24 16:46:57 +0800

    Add HardwareRenderer class for rendering data

---
+++ plugins/base/proxy/hardware/HardwareRenderer .java
@@ -0,0 +1,8 @@
+package media;
+
+public class HardwareRenderer implements Renderer {
+    @Override
+    public void render(String data) {
+        System.out.println("[Hardware Renderer] " + data);
+    }
+}
