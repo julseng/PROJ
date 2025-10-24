19ca42fc96e7d7c5b8db6ed293b7d22b1064603c
julseng <jlsronquillo3@gmail.com>
2025-10-24 16:34:33 +0800

    Add MediaPlayer class for media playback

---
+++ src/core/player/MediaPlayer.java
@@ -0,0 +1,19 @@
+package media;
+
+public class MediaPlayer {
+    private Renderer renderer;
+
+    public MediaPlayer(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public void setRenderer(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public void play(PlaylistComponent component) {
+        renderer.render("Starting playback...");
+        component.play();
+        renderer.render("Playback finished.");
+    }
+}
